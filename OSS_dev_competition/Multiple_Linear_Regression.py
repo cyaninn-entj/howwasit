4f14445ed98671ffabf6714b83ac070fa5a9ac89
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2021-08-21 19:58:42 +0900

    8.21 annotate
    Analyzing code

--- OSS_dev_competition/Multiple_Linear_Regression.py
+++ OSS_dev_competition/Multiple_Linear_Regression.py
@@ -3,29 +3,30 @@ import numpy as np
 tf.disable_v2_behavior()
 
 
-x1_data = [73., 93., 89., 96., 73.]
-x2_data = [80., 88., 91., 98., 66.]
-x3_data = [75., 93., 90., 100., 70.]
-y_data = [152., 185., 180., 196., 142.]
+x1_data = [73., 93., 89., 96., 73.] #국어점수
+x2_data = [80., 88., 91., 98., 66.] #수학점수
+x3_data = [75., 93., 90., 100., 70.] #영어점수
+y_data = [152., 185., 180., 196., 142.] #최종스코어
 
 x1 = tf.placeholder(tf.float32)
 x2 = tf.placeholder(tf.float32)
 x3 = tf.placeholder(tf.float32)
 Y = tf.placeholder(tf.float32)
+#placeholder로 저장
 
  
-
 w1 = tf.Variable(tf.random_normal([1]), name = 'weight1')
 w2 = tf.Variable(tf.random_normal([1]), name = 'weight2')
 w3 = tf.Variable(tf.random_normal([1]), name = 'weight3')
-
 b = tf.Variable(tf.random_normal([1]),name = 'bias')
+#구하려는 국어,수학,영어 점수
 
 hypothesis = x1 * w1 + x2 * w2 + x3 * w3 + b
-cost = tf.reduce_mean(tf.square(hypothesis - Y))
+#Hypothesis(y추정값) = w1*x1 + w2*x2 + w3*x3 + b
 
+cost = tf.reduce_mean(tf.square(hypothesis - Y)) 
+#추정한 값(Hypothesis)와 실제값(y)의 차이를 제곱하여 수식을 만드는 부분
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=1e-5)
-
 train = optimizer.minimize(cost)
 
 sess = tf.Session()
