1a47738f9192f3b221ef59503dc4d90429719b22
yeonwoo <83701837+cyanindy@users.noreply.github.com>
2021-08-26 23:29:03 +0900

    08.26 fix pixel

--- OSS_dev_competition/pixel_RGB.py
+++ OSS_dev_competition/pixel_RGB.py
@@ -12,14 +12,14 @@ r_list = [0]*8
 g_list = [0]*8
 b_list = [0]*8
 
-r_list[0], g_list[0], b_list[0] = rgb_im.getpixel((4, 11))
-r_list[1], g_list[1], b_list[1] = rgb_im.getpixel((16, 17))
-r_list[2], g_list[2], b_list[2] = rgb_im.getpixel((32, 8))
-r_list[3], g_list[3], b_list[3] = rgb_im.getpixel((42, 8))
-r_list[4], g_list[4], b_list[4] = rgb_im.getpixel((17, 24))
-r_list[5], g_list[5], b_list[5] = rgb_im.getpixel((32, 24))
-r_list[6], g_list[6], b_list[6] = rgb_im.getpixel((17, 34))
-r_list[7], g_list[7], b_list[7] = rgb_im.getpixel((32, 34))
+r_list[0], g_list[0], b_list[0] = rgb_im.getpixel((15, 17))
+r_list[1], g_list[1], b_list[1] = rgb_im.getpixel((35, 17))
+r_list[2], g_list[2], b_list[2] = rgb_im.getpixel((15, 30))
+r_list[3], g_list[3], b_list[3] = rgb_im.getpixel((35, 30))
+r_list[4], g_list[4], b_list[4] = rgb_im.getpixel((15, 45))
+r_list[5], g_list[5], b_list[5] = rgb_im.getpixel((35, 45))
+r_list[6], g_list[6], b_list[6] = rgb_im.getpixel((15, 60))
+r_list[7], g_list[7], b_list[7] = rgb_im.getpixel((35, 60))
 
 r_AVG = sum(r_list, 0.0) / len(r_list)
 g_AVG = sum(g_list, 0.0) / len(g_list)
